7fb8f3b0faa0fa9250019361dbf4b89eaf29f837
dugalcedo <107317025+dugalcedo@users.noreply.github.com>
2024-12-09 17:35:45 +0100

    header and nav

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,14 @@
-import './App.css'
+import Header from './components/Header/Header.jsx'
+import { Routes, Route } from 'react-router-dom'
 
 function App() {
 
   return (
     <>
-
+      <Header />
+      <main>
+        {/* Router here */}
+      </main>
     </>
   )
 }
