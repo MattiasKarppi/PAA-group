9575e4a7fb42802475d12b7561745a7cf0bb5bbc
dugalcedo <dougalcedo@gmail.com>
2024-12-13 09:52:00 +0100

    home events styling

--- src/pages/Home/HomeEvents.jsx
+++ src/pages/Home/HomeEvents.jsx
@@ -1,20 +1,35 @@
 import { useCalendarContext } from '../../context/CalendarContext.jsx'
+import { Link } from 'react-router-dom'
+import sty from './HomeEvents.module.css'
 
 function HomeEvents() {
     const calendarContext = useCalendarContext()
     const upcomingEvents = calendarContext.getAllEventsAsArray().slice(0, 4)
 
     return (
-        <div id='events'>
+        <div id={sty.events}>
             <h3>Upcoming events</h3>
-            {upcomingEvents.map(evt => {
-                const key = `${evt.startDate}T${evt.startTime}`
-                return (
-                    <div key={key}>
-                        {evt.title}
-                    </div>
-                )
-            })}
+            <table id={sty.table}>
+                <thead>
+                    <th>Title</th>
+                    <th>Start date</th>
+                </thead>
+                <tbody>
+                    {upcomingEvents.map(evt => {
+                        const key = `${evt.startDate}T${evt.startTime}`
+                        return (
+                            <tr key={key}>
+                                <td>{evt.title}</td>
+                                <td>{evt.startDate}</td>
+                            </tr>
+
+                        )
+                    })}
+                </tbody>
+            </table>
+            <Link to="/calendar">
+                See all &rarr;
+            </Link>
         </div>
     )
 }
